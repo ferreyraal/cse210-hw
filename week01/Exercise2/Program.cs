a6a4d8c13802f424f829d4120d3346fda5e9b1eb
Alejandro Ferreyra <alejandroferreyra@MacBook-Air-de-Alejandro.local>
2025-03-04 18:56:47 -0300

    program finished

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("This is the Exercise 2 Project.");
+        string letter = "";
+        int grade = 0;
+        Console.Write("What is your percentage grade? ");
+        string grade_str = Console.ReadLine();
+        grade = Convert.ToInt32(grade_str);
+        if (grade >= 90)
+            {
+                letter = "A";
+            }
+        else if (grade >= 80)
+            {
+                letter = "B";
+            }
+        else if (grade >= 70)
+            {
+                letter = "C";
+            }
+        else if (grade >= 60)
+            {
+                letter = "D";
+            }
+        else 
+            {
+                letter = "F";
+            }
+        
+        Console.WriteLine($"Your letter grade is: {letter}");
+        if (grade >=70)
+            {
+                Console.WriteLine("Congratulations!  You pass the course.");
+            }
+        else
+            {
+                Console.WriteLine("You fail the course. You will have another opportunity");
+            }
+
     }
 }
