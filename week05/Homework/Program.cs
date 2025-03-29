733f16979c14d25bae3e269da81e40f35a14f885
ferreyraal <ferreyraal@gmail.com>
2025-03-29 19:14:58 -0300

    week5 homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.Clear();
+        Console.WriteLine("This is the Homework Project.");
+        Console.WriteLine("");
+        Assignment assigment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assigment1.GetSummary());
+        Console.WriteLine("");        
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine("");
+
+        WritingAssignment writing1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
+
     }
 }
